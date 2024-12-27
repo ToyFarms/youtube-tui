0f56c6c70762a2455507a07c1709797de4772d52
ToyFarms <ToyFarms@users.noreply.github.com>
2024-12-27 20:25:34 +0700

    Add image resizing function

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 from collections.abc import Generator
 from contextlib import contextmanager
 from typing import Any
+from PIL import Image
 
 import os
 import sys
@@ -106,3 +107,49 @@ def suppress_portaudio_error() -> Generator[None, None, None]:
 # shuts pyright
 def expect[T](value: Any, _: type[T]) -> T:
     return value
+
+
+# pyright: reportUnknownMemberType=false
+def resize_image(
+    image: Image.Image,
+    target_width: int | None = None,
+    target_height: int | None = None,
+) -> Image.Image:
+    if target_width is None and target_height is None:
+        raise ValueError(
+            "At least one of target_width or target_height must be specified"
+        )
+
+    orig_width, orig_height = image.size
+    aspect_ratio = orig_width / orig_height
+
+    if target_width and target_height:
+        new_size = (target_width, target_height)
+        resized = image.resize(new_size, Image.Resampling.LANCZOS)
+        return resized
+
+    elif target_width:
+        new_width = target_width
+        new_height = int(new_width / aspect_ratio)
+        resized = image.resize((new_width, new_height), Image.Resampling.LANCZOS)
+
+        if new_height < target_width:
+            padded = Image.new("RGB", (new_width, target_width), (0, 0, 0))
+            y_offset = (target_width - new_height) // 2
+            padded.paste(resized, (0, y_offset))
+            return padded
+        return resized
+
+    elif target_height:
+        new_height = target_height
+        new_width = int(new_height * aspect_ratio)
+        resized = image.resize((new_width, new_height), Image.Resampling.LANCZOS)
+
+        if new_width < target_height:
+            padded = Image.new("RGB", (target_height, new_height), (0, 0, 0))
+            x_offset = (target_height - new_width) // 2
+            padded.paste(resized, (x_offset, 0))
+            return padded
+        return resized
+
+    return image
